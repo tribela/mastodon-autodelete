500cc24443c42cc9665b2a394d6e0c66fc422f3f
Jeong Arm <kjwonmail@gmail.com>
2022-08-08 17:57:09 +0900

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+build_path=$(mktemp -d)
+# shellcheck disable=SC2064
+trap "rm -rf $build_path" EXIT
+rsync -au --info=progress2 --delete --filter=':- .gitignore' ./ "$build_path"
+cd "$build_path" || exit 1
+git reset --
+git checkout .
+git clean -fdx
+docker buildx build --pull -t "kjwon15/mastodon-autodelete" . "$@"
